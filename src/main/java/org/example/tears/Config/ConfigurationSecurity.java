cb2773b8b97461f9bd99c24ce0bd79becec898e3
AbeerAdel <Aboor.1048@gmail.com>
2026-05-21 23:47:52 +0300

    السيكيوريتي

--- src/main/java/org/example/tears/Config/ConfigurationSecurity.java
+++ src/main/java/org/example/tears/Config/ConfigurationSecurity.java
@@ -58,7 +58,6 @@ public class ConfigurationSecurity {
                                 "/api/v1/tears/users/employee/profile",
                                 "/api/v1/tears/users/update",
                                 "/api/v1/tears/users/notifications",
-                                "/api/v1/tears/location/add",
                                 "/api/v1/tears/users/change-phone/**"
                         ).authenticated()
 
@@ -72,7 +71,9 @@ public class ConfigurationSecurity {
                                 "/api/v1/tears/cars/delete/**",
                                 "/api/v1/tears/cars/details/**",
                                 "/api/v1/tears/cars/extract-owner",
+                                "/api/v1/tears/location/**",
                                 "/api/v1/tears/cars/extract-user-name"
+
                         ).hasRole("CUSTOMER")
 
                         // ================= EMPLOYEE =================
