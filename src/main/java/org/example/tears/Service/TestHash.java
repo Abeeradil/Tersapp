50350a080c9514ca541c7324b132132135cc4d15
AbeerAdel <Aboor.1048@gmail.com>
2026-06-18 23:38:55 +0300

    رمز لالا

---
+++ src/main/java/org/example/tears/Service/TestHash.java
@@ -0,0 +1,10 @@
+package org.example.tears.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class TestHash {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashed = encoder.encode("lala@1234");
+        System.out.println(hashed);
+    }
+}
