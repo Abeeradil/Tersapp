79263cb78f249f38d68fe49cdf59ee4bb2c9d0c8
AbeerAdel <Aboor.1048@gmail.com>
2026-06-30 21:55:36 +0300

    تحديث الحالات

--- src/main/java/org/example/tears/DTO/PartDto.java
+++ src/main/java/org/example/tears/DTO/PartDto.java
@@ -8,6 +8,4 @@ public class PartDto {
         private String name;
         private String type;           // جديد
         private Integer quantity;      // بدل qty
-        private String problemDescription;
-        private Integer laborCost;      // جديد
     }
