0a39ee68d4ed249ced2cc1d58b7f30fd8c989f2e
AbeerAdel <Aboor.1048@gmail.com>
2026-07-04 00:39:47 +0300

    تعديل سواقر عشان اجرب الداون لود

--- src/main/java/org/example/tears/Config/OpenApiConfig.java
+++ src/main/java/org/example/tears/Config/OpenApiConfig.java
@@ -1,10 +1,19 @@
 package org.example.tears.Config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Tears API", version = "1.0"),
+        security = {
+                @SecurityRequirement(name = "BearerAuth")
+        }
+)
 @SecurityScheme(
         name = "BearerAuth",
         type = SecuritySchemeType.HTTP,
