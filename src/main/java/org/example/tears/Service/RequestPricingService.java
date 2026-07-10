51af9c2fb270882c781b1e558e97436edad85bc8
AbeerAdel <Aboor.1048@gmail.com>
2026-07-10 06:00:35 +0300

    تعديل التقرير

--- src/main/java/org/example/tears/Service/RequestPricingService.java
+++ src/main/java/org/example/tears/Service/RequestPricingService.java
@@ -341,7 +341,7 @@ public class RequestPricingService {
         return ResponseEntity.ok()
                 .header(
                         HttpHeaders.CONTENT_DISPOSITION,
-                        "attachment; filename=pricing-report-"+request.getOrderNumber()+".pdf"
+                        "attachment; filename=pricing-report-"+report.getReportNumber()+".pdf"
                 )
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(output.toByteArray());
