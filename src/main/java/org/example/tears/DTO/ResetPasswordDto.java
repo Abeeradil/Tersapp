b00ef51311558107aaa950d02843d65a70545639
AbeerAdel <Aboor.1048@gmail.com>
2026-07-26 17:33:14 +0300

    تعديل تغيير الرمز

--- src/main/java/org/example/tears/DTO/ResetPasswordDto.java
+++ src/main/java/org/example/tears/DTO/ResetPasswordDto.java
@@ -1,6 +1,7 @@
 package org.example.tears.DTO;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -8,7 +9,16 @@ public class ResetPasswordDto {
     @NotEmpty
     private String resetToken;
     @NotEmpty
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+            message = "يجب أن تحتوي كلمة المرور على حرف كبير وصغير ورقم ورمز خاص، وألا تقل عن 8 أحرف"
+    )
     private String newPassword;
+
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+            message = "يجب أن تحتوي كلمة المرور على حرف كبير وصغير ورقم ورمز خاص، وألا تقل عن 8 أحرف"
+    )
     @NotEmpty
     private String confirmPassword;
 }
--- src/main/java/org/example/tears/InpDTO/ChangePasswordDTO.java
+++ src/main/java/org/example/tears/InpDTO/ChangePasswordDTO.java
@@ -1,14 +1,23 @@
 package org.example.tears.InpDTO;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class ChangePasswordDTO {
 
-    @NotEmpty
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+            message = "يجب أن تحتوي كلمة المرور على حرف كبير وصغير ورقم ورمز خاص، وألا تقل عن 8 أحرف"
+    )
     private String newPassword;
-    @NotEmpty
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$",
+            message = "يجب أن تحتوي كلمة المرور على حرف كبير وصغير ورقم ورمز خاص، وألا تقل عن 8 أحرف"
+    )
     private String confirmPassword;
 }
