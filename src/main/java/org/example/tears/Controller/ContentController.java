bc6633b9bd5093c42be60a1ee94cba3dd92e38cc
AbeerAdel <Aboor.1048@gmail.com>
2026-07-26 13:57:32 +0300

    سياسه الخصوصيه

--- src/main/java/org/example/tears/Controller/ContentController.java
+++ src/main/java/org/example/tears/Controller/ContentController.java
@@ -30,6 +30,22 @@ public class ContentController {
             return mapper.readValue(is, Map.class);
         }
 
+    @GetMapping("/terms-en")
+    public Map<String, Object> getTermsEn() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        InputStream is = getClass()
+                .getClassLoader()
+                .getResourceAsStream("content/terms_En.json");
+
+        if (is == null) {
+            throw new RuntimeException("terms_En.json not found");
+        }
+
+        return mapper.readValue(is, Map.class);
+    }
+
         @GetMapping("/privacy")
         public Map<String, Object> getPrivacyPolicy() throws Exception {
 
@@ -46,6 +62,38 @@ public class ContentController {
             return mapper.readValue(is, Map.class);
         }
 
+    @GetMapping("/privacy-en")
+    public Map<String, Object> getPrivacyPolicyEn() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        InputStream is = getClass()
+                .getClassLoader()
+                .getResourceAsStream("content/privacy_En.json");
+
+        if (is == null) {
+            throw new RuntimeException("privacy_En.json not found");
+        }
+
+        return mapper.readValue(is, Map.class);
+    }
+
+    @GetMapping("/privacy_policy_em")
+    public Map<String, Object> getPrivacyPolicyEm() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        InputStream is = getClass()
+                .getClassLoader()
+                .getResourceAsStream("content/Privacy_Policy_em.json");
+
+        if (is == null) {
+            throw new RuntimeException("Privacy_Policy_em.json not found");
+        }
+
+        return mapper.readValue(is, Map.class);
+    }
+
     @GetMapping("/faqs")
     public Map<String, Object> getFaqs() throws Exception {
 
@@ -57,6 +105,22 @@ public class ContentController {
 
         if (is == null) {
             throw new RuntimeException("faqs.json not found");
+        }
+
+        return mapper.readValue(is, Map.class);
+    }
+
+    @GetMapping("/faqs-en")
+    public Map<String, Object> getFaqsEn() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        InputStream is = getClass()
+                .getClassLoader()
+                .getResourceAsStream("content/faqs_En.json");
+
+        if (is == null) {
+            throw new RuntimeException("faqs_en.json not found");
         }
 
         return mapper.readValue(is, Map.class);
