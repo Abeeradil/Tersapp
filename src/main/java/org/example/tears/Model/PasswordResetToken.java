83978efe964a4c91feb17b40fb9da6d7648f7291
AbeerAdel <Aboor.1048@gmail.com>
2026-07-26 17:23:53 +0300

    تعديل تغيير الرمز

--- src/main/java/org/example/tears/Model/PasswordResetToken.java
+++ src/main/java/org/example/tears/Model/PasswordResetToken.java
@@ -1,10 +1,15 @@
 package org.example.tears.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PasswordResetToken {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
