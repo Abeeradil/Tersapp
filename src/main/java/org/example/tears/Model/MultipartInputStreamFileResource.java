a0cbc4598d50a37f13517af8d91e914f8b3e6d90
AbeerAdil <Aboor.1048@gmail.com>
2026-08-18 12:00:40 +0300

    feat: connect automatic car registration to Vision OCR

--- src/main/java/org/example/tears/Model/MultipartInputStreamFileResource.java
+++ src/main/java/org/example/tears/Model/MultipartInputStreamFileResource.java
@@ -4,28 +4,24 @@ import org.springframework.core.io.InputStreamResource;
 
 import java.io.InputStream;
 
-public class MultipartInputStreamFileResource
-        extends InputStreamResource {
+public class MultipartInputStreamFileResource extends InputStreamResource {
 
     private final String filename;
+    private final long size;
 
-    public MultipartInputStreamFileResource(
-            InputStream inputStream,
-            String filename
-    ) {
-
+    public MultipartInputStreamFileResource(InputStream inputStream, String filename, long size) {
         super(inputStream);
         this.filename = filename;
+        this.size = size;
     }
 
     @Override
     public String getFilename() {
-        return this.filename;
+        return filename;
     }
 
     @Override
     public long contentLength() {
-        return -1;
+        return size;
     }
-
 }
